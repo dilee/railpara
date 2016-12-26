91dc28d8dc8840026549fde5d7b9ba7bbaab7d8f
Dileesha Rajapakse <dilee.dev@gmail.com>
2016-12-26 15:39:58 +0530

    Refactored slider menu files and scripts for the hundredth f*****g time

--- js/menu.js
+++ js/menu.js
@@ -1,22 +1,48 @@
-$(document).on("pagecreate","#homepage",function(){
+$(document).on("pagecreate", "#homepage", function () {
     var icon = document.getElementById('menubutton');
     var open = false;
-    $("#menubuttonblock").on("tap",function(){
-        if(open){
+    $("#menubuttonblock").on("tap", function () {
+        if (open) {
             icon.className = 'menu-icon';
-        } else{
+        } else {
             icon.className = 'menu-icon open';
         }
         open = !open;
     });
 
-    $("#menu").on("panelbeforeclose",function(){
-        if(open){
+    $("#menu").on("panelbeforeclose", function () {
+        if (open) {
             icon.className = 'menu-icon';
-        } else{
+        } else {
             icon.className = 'menu-icon open';
         }
         open = !open;
+    });
+
+    $(document).on('pageinit', function(){
+        var width = $(window).width();
+        if(width > 768){
+            $("#menu").panel("open"); /* nav-panel is the id of the panel div*/
+            if (open) {
+                icon.className = 'menu-icon';
+            } else {
+                icon.className = 'menu-icon open';
+            }
+            open = !open;
+        }
+    });
+
+    $(window).resize(function () {
+        if (window.innerWidth > 768) {
+            $("#menu").panel("open");
+            open = true;
+        }
+        if (window.innerWidth < 768) {
+            $("#menu").panel("close");
+            open = false;
+        }
+        icon.className = 'menu-icon';
     });
 
 });
+
